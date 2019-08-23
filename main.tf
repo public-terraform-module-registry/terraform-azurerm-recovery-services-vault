1ad4c9c82c5b6b9b8faa4255b1cd34a846aeaec4
tomu <kusano.wireless@gmail.com>
2019-08-23 15:21:00 +0000

    fix tupo

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_recovery_services_vault" "vault" {
-  name                = "${var,recovery_services_vault_name}"
+  name                = "${var.recovery_services_vault_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   sku                 = "${var.sku}"
