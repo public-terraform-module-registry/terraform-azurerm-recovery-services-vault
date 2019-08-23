93ad501674f3ed51f6594bac96cd57bc059d5795
tomu <kusano.wireless@gmail.com>
2019-08-23 14:52:59 +0000

    fix typo

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ resource "azurerm_recovery_services_vault" "vault" {
   name                = "${var,recovery_services_vault_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
-  sku                 = "${var.sku"
+  sku                 = "${var.sku}"
 }
