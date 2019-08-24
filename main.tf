436fba27b8fbe73eec12780a9c2fc7e92a001493
tomu <kusano.wireless@gmail.com>
2019-08-24 08:26:04 +0000

    Add dependecy amoung resouces

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "azurerm_recovery_services_protection_policy_vm" "vault_policy" {
   retention_daily {
     count = "${var.count}"
   }
+
+  depends_on = ["azurerm_recovery_services_vault.vault"]
 }
 
 resource "azurerm_recovery_services_protected_vm" "protected_vm" {
@@ -31,4 +33,5 @@ resource "azurerm_recovery_services_protected_vm" "protected_vm" {
   recovery_vault_name = "${var.recovery_services_vault_name}"
   source_vm_id        = "${var.source_vm_id}"
   backup_policy_id    = "${azurerm_recovery_services_protection_policy_vm.vault_policy.id}"
+  depends_on          = ["azurerm_recovery_services_vault.vault", "azurerm_recovery_services_protection_policy_vm.vault_policy"]
 }
